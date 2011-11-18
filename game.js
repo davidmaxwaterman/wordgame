76a6b84bbb697fc13e9afaba69a1826b54577c0a
Max Waterman <max.waterman@intel.com>
2011-11-18 14:33:42 +0200

    made work with web server, and some formatting changes to make it more consistent.

--- game.js
+++ game.js
@@ -7,48 +7,48 @@ $( document ).bind( "pagecreate", function() {
         var numberCorrect = 0;
 
         $( this ).find( "thead" ).each( function( index, element ) {
-            var tr=$("<tr>");
+            var tr=$( "<tr>" );
 
             for ( var keyIndex=0; keyIndex<keys.length; keyIndex++ ) {
-                if (!ignoreKeys[keyIndex]) {
-                    var td=$("<td>")
-                        .attr("align","center")
-                        .append(keys[keyIndex]);
-                    tr.append(td);
+                if ( !ignoreKeys[ keyIndex ] ) {
+                    var td=$( "<td>" )
+                        .attr( "align", "center" )
+                        .append( keys[ keyIndex ] );
+                    tr.append( td );
                 }
             }
 
-            $(this).append(tr);
+            $( this ).append( tr );
         });
 
         $( this ).find( "tbody" ).each( function( index, element ) {
-            var tbody = $(element);
+            var tbody = $( element );
             var indexList = [];
 
             var shuffle = function( listToShuffle ) {
                 // fisher-yates shuffle
                 for ( var wordIndex=words.length-1; wordIndex>0; wordIndex-- ) {
-                    var newIndex = Math.floor(Math.random()*wordIndex);
-                    var oldIndex = listToShuffle[newIndex];
-                    listToShuffle[newIndex]  = listToShuffle[wordIndex];
-                    listToShuffle[wordIndex] = oldIndex;
+                    var newIndex = Math.floor( Math.random()*wordIndex );
+                    var oldIndex = listToShuffle[ newIndex ];
+                    listToShuffle[ newIndex ]  = listToShuffle[ wordIndex ];
+                    listToShuffle[ wordIndex ] = oldIndex;
                 }
             }
 
             for ( var keyIndex=0; keyIndex<keys.length; keyIndex++ ) {
-                if (!ignoreKeys[keyIndex]) {
-                    indexList[keyIndex] = [];
+                if ( !ignoreKeys[ keyIndex ] ) {
+                    indexList[ keyIndex ] = [];
                     for ( var wordIndex=0; wordIndex<words.length; wordIndex++ ) {
-                        indexList[keyIndex][wordIndex]=wordIndex;
+                        indexList[ keyIndex ][ wordIndex ]=wordIndex;
                     }
-                    shuffle( indexList[keyIndex] );
+                    shuffle( indexList[ keyIndex ] );
                 }
             }
 
             for ( var wordIndex=0; wordIndex<words.length; wordIndex++ ) {
                 var tr=$( "<tr>" );
                 for ( var keyIndex=0; keyIndex<keys.length; keyIndex++ ) {
-                    if (!ignoreKeys[keyIndex]) {
+                    if ( !ignoreKeys[ keyIndex ] ) {
                         var shuffledWordIndex = indexList[ keyIndex ][ wordIndex ];
                         var thisKey = keys[ keyIndex ];
                         var thisWord = words[ shuffledWordIndex ][ thisKey ];
@@ -64,8 +64,8 @@ $( document ).bind( "pagecreate", function() {
                             .html( thisText );
 
                         var thisButton = $( "<a>" )
-                            .attr( "href","#" )
-                            .attr( "data-role","button" )
+                            .attr( "href", "#" )
+                            .attr( "data-role", "button" )
                             .attr( "word-index", shuffledWordIndex )
                             .attr( "key-index", keyIndex )
                             .addClass( "ui-btn" )
@@ -76,28 +76,27 @@ $( document ).bind( "pagecreate", function() {
 
                         thisButton.click( function() {
                             var selectButton = function() {
-                                console.log( "selectButton/"+$(this).attr("key-index")+"/"+$(this).attr("word-index") );
-                                $(this)
-                            .removeClass( "ui-btn-up-c" )
-                            .addClass( "ui-btn-down-c" )
-                            .removeClass("ui-btn-corner-all");
+                                $( this )
+                                    .removeClass( "ui-btn-up-c" )
+                                    .addClass( "ui-btn-down-c" )
+                                    .removeClass( "ui-btn-corner-all" );
                             };
                             var deselectButton = function() {
-                                $(this)
-                            .addClass( "ui-btn-up-c" )
-                            .removeClass( "ui-btn-down-c" )
-                            .addClass("ui-btn-corner-all");
+                                $( this )
+                                    .addClass( "ui-btn-up-c" )
+                                    .removeClass( "ui-btn-down-c" )
+                                    .addClass( "ui-btn-corner-all" );
                             };
 
-                            var thisWordIndex = $(this).attr("word-index");
-                            var thisKeyIndex = $(this).attr("key-index");
-                            if (noWordSelected) {
-                                $(this).each( selectButton );
+                            var thisWordIndex = $( this ).attr( "word-index" );
+                            var thisKeyIndex = $( this ).attr( "key-index" );
+                            if ( noWordSelected ) {
+                                $( this ).each( selectButton );
 
                                 noWordSelected = false;
                                 currentlySelectedWord = thisWordIndex;
 
-                                keySelected[ thisKeyIndex ] = $(this);
+                                keySelected[ thisKeyIndex ] = $( this );
 
                                 // highlight in green
                                 console.log( "green/"+thisWordIndex+"/"+thisKeyIndex+"/" );
@@ -105,12 +104,12 @@ $( document ).bind( "pagecreate", function() {
                                 if ( keySelected[ thisKeyIndex ] ) {
                                     // user changed mind
                                     // deselect all buttons for this key
-                                    tbody.find("a[key-index="+thisKeyIndex+"]").each( deselectButton );
+                                    tbody.find( "a[key-index="+thisKeyIndex+"]" ).each( deselectButton );
                                     // select new one
-                                    $(this).each( selectButton );
+                                    $( this ).each( selectButton );
 
                                     currentlySelectedWord = thisWordIndex;
-                                    keySelected[ thisKeyIndex ] = $(this);
+                                    keySelected[ thisKeyIndex ] = $( this );
 
                                     // highlight in green
                                     console.log( "amber/"+thisWordIndex+"/"+thisKeyIndex+"/" );
@@ -118,11 +117,11 @@ $( document ).bind( "pagecreate", function() {
                                     var thisKey = keys[ thisKeyIndex ];
                                     console.log( "1/"+thisWordIndex+"/"+currentlySelectedWord+"/" );
                                     console.log( "1/"+words[thisWordIndex][thisKey]+"/"+words[currentlySelectedWord][thisKey]+"/" );
-                                    var correct = words[thisWordIndex][thisKey] === words[currentlySelectedWord][thisKey];
+                                    var correct = words[ thisWordIndex ][ thisKey ] === words[ currentlySelectedWord ][ thisKey ];
                                     if ( correct ) {
-                                        $(this).each( selectButton );
+                                        $( this ).each( selectButton );
 
-                                        keySelected[ thisKeyIndex ] = $(this);
+                                        keySelected[ thisKeyIndex ] = $( this );
 
                                         // highlight in green
                                         console.log( "correct/"+thisWordIndex+"/"+currentlySelectedWord+"/" );
@@ -131,8 +130,8 @@ $( document ).bind( "pagecreate", function() {
                                             var retVal = true;
 
                                             for ( var keyIndex=0; keyIndex<keys.length; keyIndex++ ) {
-                                                if (!ignoreKeys[keyIndex]) {
-                                                    retVal &= (keySelected[ keyIndex ]!=null);
+                                                if ( !ignoreKeys[keyIndex] ) {
+                                                    retVal &= ( keySelected[ keyIndex ]!=null );
                                                 }
                                             }
 
@@ -140,34 +139,34 @@ $( document ).bind( "pagecreate", function() {
                                         }
 
                                         if ( allKeysSelected() ) {
-                                            console.log("all correct");
+                                            console.log( "all correct" );
 
                                             noWordSelected = true;
                                             for ( var keyIndex=0; keyIndex<keys.length; keyIndex++ ) {
-                                                if (!ignoreKeys[keyIndex]) {
+                                                if ( !ignoreKeys[ keyIndex ] ) {
                                                     var buttonToRemove = keySelected[ keyIndex ];
-                                                    var buttonTd = buttonToRemove.closest("td");
+                                                    var buttonTd = buttonToRemove.closest( "td" );
 
                                                     var nextButton = buttonTd // <td>
-                                                        .closest("tr") // <tr>
-                                                        .next("tr") // next <tr>
-                                                        .find("[key-index="+keyIndex+"]"); // next <a> with the correct key-index
-                                                    var nextButtonWordIndex = nextButton.attr("word-index");
+                                                        .closest( "tr" ) // <tr>
+                                                        .next( "tr" ) // next <tr>
+                                                        .find( "[key-index="+keyIndex+"]" ); // next <a> with the correct key-index
+                                                    var nextButtonWordIndex = nextButton.attr( "word-index" );
 
                                                     buttonToRemove.remove();
 
-                                                    while (nextButton.length) {
-                                                        var nextButtonTd = nextButton.closest("td");
+                                                    while ( nextButton.length ) {
+                                                        var nextButtonTd = nextButton.closest( "td" );
 
                                                         // move next button up into td
                                                         buttonTd.append( nextButton );
 
                                                         // find next button
                                                         nextButton = nextButtonTd
-                                                            .closest("tr") // <tr>
-                                                            .next("tr") // next <tr>
-                                                            .find("[key-index="+keyIndex+"]"); // next <a> with the correct key-index
-                                                        nextButtonWordIndex = nextButton.attr("word-index");
+                                                            .closest( "tr" ) // <tr>
+                                                            .next( "tr" ) // next <tr>
+                                                            .find( "[key-index="+keyIndex+"]" ); // next <a> with the correct key-index
+                                                        nextButtonWordIndex = nextButton.attr( "word-index" );
 
                                                         // move td to next button's td
                                                         buttonTd = nextButtonTd;
@@ -178,14 +177,14 @@ $( document ).bind( "pagecreate", function() {
                                             }
 
                                             // remove last tr
-                                            tbody.find("tr").last().remove();
+                                            tbody.find( "tr" ).last().remove();
 
                                             numberCorrect++;
                                             document.title = "Game "+numberCorrect+"/"+words.length;
                                         }
                                     } else {
                                         // highlight in red?
-                                        alert( "hint: "+words[currentlySelectedWord][thisKey] );
+                                        alert( "hint: "+words[ currentlySelectedWord ][ thisKey ] );
                                     }
                                 }
                             }
